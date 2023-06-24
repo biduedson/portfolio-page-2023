e4f38534c124d1c33aa6ec740cca556e562da4d0
biduedson <112134265+biduedson@users.noreply.github.com>
2023-06-23 21:14:47 -0300

    Criando componentes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3300',
+        changeOrigin: true
+
+      },
+
+    }
+  }
+
 })
+
+
+
